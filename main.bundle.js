6e51500334c649c619271343c8be46db97117471
DoraChad <dragazolt2411@gmail.com>
2025-04-27 20:38:43 -0700

    _

--- main.bundle.js
+++ main.bundle.js
@@ -137,7 +137,7 @@ const interval = setInterval(updateCountdown, 1000);
 
 const leaderboardUI = function() {
 
-const main = document.getElementById("ui")[0];
+const main = document.getElementById("ui");
 
 const md = document.createElement("div");
 md.style.display = "flex";
